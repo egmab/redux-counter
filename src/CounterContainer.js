5d7190a79b5e15e8e21caa7d91a9ec8445e38142
eugenie <Vous@exemple.com>
2019-06-03 14:40:25 +0200

    commit elements

---
+++ src/CounterContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  count: state
+});
+
+const CounterComponent = ({ count, dispatch }) => (
+  <div>
+    <p>{count}</p>
+    <button onClick={() => dispatch({ type: "ADD" })}>+ 1</button>
+    <button onClick={() => dispatch({ type: "REMOVE" })}>- 1</button>
+    <button onClick={() => dispatch({ type: "ADD10" })}>+ 10</button>
+    <button onClick={() => dispatch({ type: "REMOVE10" })}>-10</button>
+    <button onClick={() => dispatch({ type: 'RESET'})}>reset</button>
+  </div>
+);
+
+export default connect(
+  mapStateToProps,
+)(CounterComponent);
